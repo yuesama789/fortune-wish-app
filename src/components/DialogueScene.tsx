2204e7c66c1eb048e89deac72b40607691cb7085
yuesama789 <84022757+yuesama789@users.noreply.github.com>
2026-03-23 11:45:26 +0100

    feat: add new dialogue background images and update background handling in DialogueScene component

--- src/components/DialogueScene.tsx
+++ src/components/DialogueScene.tsx
@@ -54,11 +54,22 @@ const DialogueScene: React.FC<DialogueSceneProps> = ({ character, onWishClick, o
         </svg>
     );
 
+    const returnIcon = (
+        <svg fill="#ffffff" height="32px" width="32px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512" xmlSpace="preserve">
+        <g>
+            <g>
+                <path d="M320,112H192V48h-32L0,208l160,160h32v-64h160c88.365,0,112,71.635,112,160h48V304C512,197.962,426.038,112,320,112z"/>
+            </g>
+        </g>
+        </svg>
+    );
+
     const handIcon = (
         <svg fill="#ffffff" width="32px" height="32px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.924 17.315c-.057.174-.193.367-.416.432-.161.047-5.488 1.59-5.652 1.633-.469.125-.795.033-1.009-.156-.326-.287-4.093-2.85-8.845-3.092-.508-.025-.259-1.951 1.193-1.951.995 0 3.904.723 4.255.371.271-.272.394-1.879-.737-4.683L4.438 4.232a1.045 1.045 0 0 1 1.937-.781L8.361 8.37c.193.48.431.662.69.562.231-.088.279-.242.139-.709L7.144 2.195A1.043 1.043 0 0 1 7.796.871a1.042 1.042 0 0 1 1.325.652l1.946 5.732c.172.504.354.768.642.646.173-.073.161-.338.115-.569l-1.366-5.471a1.045 1.045 0 1 1 2.027-.506l1.26 5.042c.184.741.353 1.008.646.935.299-.073.285-.319.244-.522l-.872-4.328a.95.95 0 0 1 1.86-.375l.948 4.711.001.001v.001l.568 2.825c.124.533.266 1.035.45 1.527 1.085 2.889.519 5.564.334 6.143z"/></svg>
     );
     return (
-        <div className="dialogue-scene" style={{ backgroundImage: `url(/images/dialogue_assets/background/Liyue.png)` }}>
+        <div className="dialogue-scene" style={{ backgroundImage: `url(/images/dialogue_assets/background/${character.region}.webp)` }}>
             <img src={`/images/dialogue_assets/character/5/${character.name}.png`} alt={character.name} className="character-image" />
             <div className='dialogue-blurredbackground'>
                 <div className='dialogue-container'>
@@ -81,7 +92,7 @@ const DialogueScene: React.FC<DialogueSceneProps> = ({ character, onWishClick, o
                     onClick={() => triggerOption('return', onReturnClick)}
                 >
                     <span className='dialogue-options-border'></span>
-                    {speechBubbleIcon}
+                    {returnIcon}
                     (Select another character)
                 </div>
             </div>

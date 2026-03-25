2890ab1e3f4e844ac7d73c54222ef23fe82c1ae6
yuesama789 <84022757+yuesama789@users.noreply.github.com>
2026-03-25 21:21:59 +0100

    correct base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/genshin-fortune-slip-wish-app/',
+  base: '/fortune-wish-app/',
   server: {
     port: 3000,
     open: true,
